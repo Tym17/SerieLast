0fe15cb6c701562d6d3c4f8cddf104fc32c36bb9
Tym17 <Tym17@users.noreply.github.com>
2016-09-26 11:30:41 +0200

    Uppercase file names

---
+++ library/SqliteHandle.php
@@ -0,0 +1,56 @@
+<?php
+
+class SqliteHandle
+{
+    protected $dbhandle;
+
+    function __construct($db_filename)
+    {
+        $this->dbhandle = new SQLite3($db_filename);
+
+        if (!$this->dbhandle)
+            die ('Could not connect to db');
+    }
+
+    function __destruct()
+    {
+        $this->dbhandle->close();
+    }
+
+    function query($query)
+    {
+        $result = $this->dbhandle->query($query);
+        if (!$result)
+        {
+            return false;
+        }
+        return $result;
+    }
+
+    function arrayify($result)
+    {
+      // Arrayification of results
+      $retArray = array();
+      $inserted = true;
+      // While there is items to be inserted
+      while ($inserted)
+      {
+          $inserted = $result->fetchArray(SQLITE3_ASSOC);
+          if ($inserted)
+          {
+              // An item has been fetched
+
+              $retArray[] = $inserted;
+          }
+      }
+      return $retArray;
+    }
+
+    // Escape malicious characters
+    function es($str)
+    {
+      $str = htmlspecialchars($str);
+      $str = str_replace("'", "''", $str);
+      return $str;
+    }
+}
---
+++ library/controllers/Controller.php
@@ -0,0 +1,42 @@
+<?php
+class Controller {
+
+    protected $_controller;
+    protected $_action;
+    protected $_template;
+
+    function __construct($controller, $action) {
+
+        $this->_controller = $controller;
+        $this->_action = $action;
+        $this->_template = new Template($controller,$action);
+    }
+
+    /*
+    ** Pass variable to the Template
+    */
+    function set($name,$value) {
+        $this->_template->set($name,$value);
+    }
+
+    function __destruct() {
+        // render Template uppon destruct
+        $this->_template->render();
+    }
+
+    /*
+    ** Basic function call if no action is specified while calling for this controller
+    */
+    function index()
+    {
+
+    }
+
+    /*
+    ** Controller's own 404error page
+    */
+    function noAction()
+    {
+        echo 'noAction';
+    }
+}
